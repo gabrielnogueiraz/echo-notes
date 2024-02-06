c04ccd6f558d57e8fa6e2495a5adbb4069b38fef
Gabriel <108474013+gabrielnogueiraz@users.noreply.github.com>
2024-02-05 22:44:28 -0300

    user interface implementation"

---
+++ src/app.tsx
@@ -0,0 +1,28 @@
+import logo from './assets/logo-echo-notes.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
+export function App() {
+  return (
+    <div className='mx-auto max-w-6xl mt-12 space-y-6'>
+      <img src={logo} alt="Echo Notes" />
+      <form className='w-full'>
+        <input
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500' 
+          type="text" 
+          placeholder='Busque em suas notas...'
+        />
+      </form>
+
+      <div className='h-px bg-slate-700'></div>
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
+  )
+}
